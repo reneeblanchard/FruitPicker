23ba3b63428c6464b3c81b16e74bc859bd07463f
Renee Blanchard <renee.blanchard@capgemini.com>
2017-01-26 10:51:08 +0000

    release 0.0.2

---
+++ FruitPicker/Build/Intermediates/FruitPicker.build/Debug-iphonesimulator/FruitPicker.build/Objects-normal/x86_64/FruitPicker_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/reneeblanchard/Documents/Heathrow/Cocoapod/FruitPicker/FruitPicker/Build/Intermediates/FruitPicker.build/Debug-iphonesimulator/FruitPicker.build/DerivedSources/FruitPicker_vers.c
